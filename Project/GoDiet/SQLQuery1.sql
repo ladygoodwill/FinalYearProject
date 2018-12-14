728cb47f0304aecd2e78007877b2011b0b8caba1
Judyta <Judyta.Dabek@gmail.com>
2018-12-14 11:40:16 +0000

    introduced new changes, final report and recipes

---
+++ Project/GoDiet/SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿USE [C:\USERS\OWNER\DOCUMENTS\GODIETCUSTINFO.MDF]
+GO
+
+--DECLARE	@return_value Int
+
+--EXEC	@return_value = [dbo].[GenderUpdate]
+--		@Username = N'',
+--		@Gender = N''
+
+--SELECT	@return_value as 'Return Value'
+--SELECT * from [dbo].tblUserNamePassw
+select * from tblBreakfast
+--delete from tblBreakfast
+--delete from tblUserNamePassw
+--DELETE FROM [dbo].tblUserNamePassw
+GO
---
+++ Project/GoDiet/dbo.tblBreakfast.data.sql
@@ -0,0 +1,280 @@
+﻿INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+2,
+'Sandwiches with cucumber-olive cottage cheese',
+435,
+'Using fork mash cheese and yogurt. Cucumber and olives cut and add to cottage cheese, add basil and spread on sandwiches',
+'Yes',
+'105g Bread,
+90g Cottage Cheese,
+75g Natural Yogurt,
+24g Olives,
+85g Cucumber,
+2g Herbs',
+381,
+29,
+68,
+8
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+3,
+'Spelled Groats with Strawberries and Almonds',
+460,
+'Boil spelled groat for few minutes in almond milk, wash and blend strawberries, add honey, mix all ingredients. Serve.',
+'Yes',
+'220g Almond milk,
+52g Spelled groat,
+160g Strawberries,
+10g Honey,
+20g Almonds',
+462,
+19,
+69,
+16
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+4,
+'Yogurt omlette with fruits',
+456,
+'Beat egg into bowl, add flour and yogurt. Blend. Spill mass on frying pan, each side fry for 3 mins. Place fruits on the top.',
+'Yes',
+'2 Eggs,
+15g Wheat Flour,
+60g Natural Yogurt,
+185g Banana,
+100g Strawberries',
+480,
+22,
+66,
+14
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+5,
+'Sandwiches With Lettuce, White Cheese and Carrot',
+441,
+'Spread butter on bread slices, lay on lettuce and cheese, sprinkle with grated carrot.',
+'Yes',
+'105g Bread,
+10g Butter,
+15g Lettuce,
+90g Cottage Cheese,
+110g Carrot',
+330,
+26,
+70,
+11
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+6,
+'Fried Eggs With Pumpkin Bits and Sunflower Seeds',
+452,
+'Wash and cut pumpkin, fry on oil. Stick eggs on frying pan, add salt, pepper and herbs, fry for few mins, add chives and sunflower seeds.',
+'Yes',
+'3 Eggs,
+200g Pumpkin,
+10g Oil,
+10g Chives,
+10g Sunflower seeds,
+4g Herbs',
+414,
+28,
+19,
+33
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+7,
+'Cottage Cheese Made of Tofu with Raddishes',
+458,
+'Mince tofu with fork, add yogurt, mix it. Add cut raddishes and herbs. Eat with bread.',
+'Yes',
+'240g Tofu,
+160g Natural Yogurt,
+120g Raddishes,
+4g Herbs,
+70g Bread',
+594,
+32,
+58,
+16
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+8,
+'Cottage Cheese with Nuts and Tomato and Bread',
+432,
+'Cut tomato, mix with crushed nuts, chives and cheese. Add pepper. Eat with bread.',
+'Yes',
+'100g Natural Cottage Cheese,
+12g Nuts,
+65g Tomato,
+4g Chives,
+105g Bread',
+286,
+21,
+63,
+14
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+9,
+'Ginger Porridge with Chia seeds',
+436,
+'Cook oats, ginger and chia in almond milk until will boiling, then hold on smaller fire for next 10 mins. Enjoy.',
+'Yes',
+'220g Almond Milk,
+70g Oats,
+15g Chia Seeds,
+5g Herbs',
+310,
+19,
+61,
+15
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+10,
+'Muesli with Buttermilk, Mandarine, Kiwi',
+452,
+'Add muesli to buttermilk, add mandarine and kiwi, blender.',
+'Yes',
+'250g Natural Buttermilk,
+50g Muesli,
+120g Mandarine,
+220g Kiwi',
+640,
+17,
+87,
+9
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+11,
+'Guacamole Paste',
+436,
+'Mince avocado, add minced garlic, lemon juice and salt, serve with bread.',
+'Yes',
+'160g Avocado,
+6g Lemon,
+2g Herbs,
+10g Garlic,
+70g Bread',
+248,
+9,
+53,
+26
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+12,
+'Toasts with Cottage Cheese and Pineapple',
+458,
+'Spread butter on bread, warm up in oven for 5 mins. Spread cheese and put pineapple slices on the top.',
+'Yes',
+'105g Bread,
+90g Cottage Cheese,
+90g Pineapple,
+10g Butter',
+295,
+25,
+72,
+11
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+13,
+'Toasts with Cottage Cheese, Tomato and Basil',
+444,
+'Spread olive on bread, warm up in oven for 5 mins. Spread cottage cheese on bread, lay sliced tomato on top. Sprinkle basil.',
+'Yes',
+'130g Tomato,
+105g Bread,
+90g Cottage Cheese,
+10g Olive,
+2g Herbs',
+337,
+26,
+64,
+13
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+14,
+'Scrambled Eggs with Chives and Toast',
+460,
+'Fry eggs on oil together with chives, toast warm up in toaster or oven.',
+'Yes',
+'2 Eggs,
+105g Bread,
+8g Chives,
+5g Oil',
+238,
+22,
+58,
+19
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+15,
+'Oatmeal, Nut Butter and Bananas',
+394,
+'Slice medium-size banana over the cooked oatmeal, add a tablespoon of peanut butter.',
+'Yes',
+'100g Oatmeal,
+medium-size banana,
+1 tbls peanut butter',
+NULL,
+9,
+59,
+12
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+16,
+'Smoothie With Greek Yogurt',
+198,
+'Combine a cup of strawberries with one banana, a cup of spinach and a half-cup of nonfat Greek yogurt in a blender and blend it up.',
+'Yes',
+'100g Strawberries,
+1 banana,
+1 cup of spinach,
+1/2 cup Greek Yogurt',
+NULL,
+12,
+43,
+2
+)
+
+INSERT INTO [dbo].[tblBreakfast] ([BreakID], [BreakName], [CaloriesNo], [Description], [VeggieOption], [Ingredients], [Gram], [Proteins], [Carbons], [Fats])
+values (
+17,
+'Toast With Egg and Oranges',
+393,
+'Top each slice with an egg. Add a cup of orange sections for extra carbs and fiber on the side.',
+'Yes',
+'2 slices of whole-grain toast,
+2 eggs,
+1 cup of oranges',
+NULL,
+21,
+43,
+10
+)
